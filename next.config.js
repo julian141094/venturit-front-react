354a2ce9c8c9595e583915f5b445505a134a7e34
Julian Avendaño <julian.arkangel@gmail.com>
2021-11-26 21:37:01 -0400

    fixed static files build bug

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
